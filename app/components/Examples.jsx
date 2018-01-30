0d1572c51d9146f2fed490eab22be90568b87c98
imamaris <imam.aris94@gmail.com>
2018-01-30 12:36:57 +0700

    fixing example component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
 
-var Examples  = (props) => {
+var Examples = (props) => {
     return (
-        <h3>Examples Component!</h3>
-        <p>Welcome to examples page!</p>
+        <div>
+            <h3>Examples Component!</h3>
+            <p>Welcome to examples page!</p>
+        </div>
     )
 };
 
